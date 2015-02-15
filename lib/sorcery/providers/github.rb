f47dc879e93b0011c4477629d41034c5ca767f38
wendy kurniawan soesanto <wendy.kurniawan@veritrans.co.id>
2015-02-15 23:15:06 +0700

    Revert "update"

--- lib/sorcery/providers/github.rb
+++ lib/sorcery/providers/github.rb
@@ -26,9 +26,7 @@ module Sorcery
         response = access_token.get(user_info_path)
 
         auth_hash(access_token).tap do |h|
-          h[:user_info] = JSON.parse(response.body).tap do |uih|
-            uih['email'] = primary_email(access_token) if scope =~ /user/
-          end
+          h[:user_info] = JSON.parse(response.body)
           h[:uid] = h[:user_info]['id']
         end
       end
@@ -46,13 +44,6 @@ module Sorcery
         end
 
         get_access_token(args, token_url: token_url, token_method: :post)
-      end
-
-      def primary_email(access_token)
-        response = access_token.get(user_info_path + "/emails")
-        emails = JSON.parse(response.body)
-        primary = emails.find{|i| i['primary'] }
-        primary && primary['email'] || emails.first && emails.first['email']
       end
 
     end
